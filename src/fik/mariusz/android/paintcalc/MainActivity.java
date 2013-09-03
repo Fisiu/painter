938c78964ce1e9780518e50ec5f58d9cea9aa0d1
Mariusz Fik <fisiu@opensuse.org>
2013-09-03 18:09:23 +0200

    Enable removing of previously added room and/or all rooms.
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

--- src/fik/mariusz/android/paintcalc/MainActivity.java
+++ src/fik/mariusz/android/paintcalc/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	private Button mButtonAddRoom;
 	private Button mButtonReset;
+	private Button mButtonRemoveLast;
 
 	private double w, l, h;
 
@@ -44,6 +45,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		mButtonAddRoom.setOnClickListener(this);
 		mButtonReset = (Button) findViewById(R.id.button_reset);
 		mButtonReset.setOnClickListener(this);
+		mButtonRemoveLast = (Button) findViewById(R.id.button_remove_last);
+		mButtonRemoveLast.setOnClickListener(this);
 
 		mRooms = (TextView) findViewById(R.id.rooms);
 		mTotal = (TextView) findViewById(R.id.total);
@@ -66,7 +69,10 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivityForResult(intent, ADD_NEW_ROOM);
 			break;
 		case R.id.button_reset:
-			// TODO add reset code
+			removeAllRooms();
+			break;
+		case R.id.button_remove_last:
+			removeLastRoom();
 			break;
 		default:
 			break;
@@ -101,7 +107,16 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	private void removeLastRoom() {
-		// TODO Remove the latest added room
+		if (!roomList.isEmpty()) {
+			// we need last room to get area size
+			Room last = roomList.get(roomList.size() - 1);
+			// remove room from list
+			roomList.remove(last);
+			// substract last room area from total area
+			setTotal(getTotal() - last.totalArea());
+			// update UI
+			updateUI();
+		}
 	}
 
 	/** Clear the list with all rooms */
@@ -109,8 +124,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		if (!roomList.isEmpty()) {
 			roomList.clear();
 			setTotal(0.0);
+			updateUI();
 		}
-		updateUI();
 	}
 
 	/** update UI labels */
